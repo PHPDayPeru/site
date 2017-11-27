80d9638b03066402106ba7e99786c52f3a4d3078
Francis Gonzales <fgonzalestello91@gmail.com>
2017-11-26 21:45:28 -0500

    PHP Day 2017 page

--- js/default.js
+++ js/default.js
@@ -35,46 +35,21 @@ function googleMap() {
   var map = document.getElementById('google-map');
 
   var map_options = {
-    center: new google.maps.LatLng(-12.0144215,-77.0540592),
+    center: new google.maps.LatLng(-12.1953946,-76.9728274),
     scrollwheel: false,
-    zoom: 12,
+    zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   var map = new google.maps.Map(map, map_options)
 
   // Map Marker
-  var myLatlng = new google.maps.LatLng(-12.0144215,-77.0540592);
+    /*
+  var myLatlng = new google.maps.LatLng(-12.1953946,-76.9728274);
   var marker = new google.maps.Marker({
     position: myLatlng,
     map: map,
     icon: 'img/location-pin.svg'
   });
+  */
 }
 google.maps.event.addDomListener(window, 'load', googleMap);
-
-$(function(){
-
-    $('a[href*=#].smooth-scroll').click(function(e) {
-
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        && location.hostname == this.hostname) {
-
-            var $target = $(this.hash);
-
-            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-
-            if ($target.length) {
-
-                var targetOffset = $target.offset().top;
-
-                $('html,body').animate({scrollTop: targetOffset}, 1000);
-
-                return false;
-
-          }
-
-      }
-
-  });
-
-});
